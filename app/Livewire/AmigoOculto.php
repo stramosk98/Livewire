c0b4e8c61bdd1fe5ad2a0c7793bdb526a343a810
Antonio S Stramosk <antoniostramos_@hotmail.com>
2024-12-24 22:55:42 -0300

    feat: first commit

---
+++ app/Livewire/AmigoOculto.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Livewire;
+
+use Illuminate\Support\Collection;
+use Livewire\Component;
+
+class AmigoOculto extends Component
+{
+    public $contacts = [];
+    public $name = '';
+    public $email = '';
+    public function addContact()
+    {
+        $this->contacts[] = ['name' => $this->name, 'email' => $this->email];
+    }
+    public function removeContact($index = 2)
+    {
+        $this->contacts = collect($this->contacts)->filter(function ($value, $key) use ($index) {
+            return $key != $index;
+        });
+    }
+    public function render()
+    {
+        return view('livewire.amigo-oculto');
+    }
+}
---
+++ resources/views/livewire/amigo-oculto.blade.php
@@ -0,0 +1,57 @@
+<div class="border">
+    {{-- The whole world belongs to you. --}}
+    <form wire:submit="addContact">
+    <div class="row m-2">
+        <div class="col-4">
+            <label class="form-label" for="apelido">Apelido</label>
+            <input class="form-control" type="text" wire:model="name">
+        </div>
+        <div class="col-5">
+            <label class="form-label" for="email">E-mail</label>
+            <input class="form-control" type="email" wire:model="email">
+        </div>
+    </div>
+
+    <button type="submit" class="m-4 button button-primary">Adicionar</button>
+
+    </form>
+
+    <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
+        <div class="text-center text-sm sm:text-left">
+            <h3>Pessoas Adicionadas</h3>
+            <table class="table table-hover bg-white rounded-3">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>E-mail</th>
+                        <th>Ações</th>
+                    </tr>
+                    <tbody>
+                        @foreach ($contacts as $index => $contact)
+                        <tr>
+                            <td class="mb-3">
+                                <span>
+                                    {{ $contact['name'] }}
+                                </span>
+                            </td>
+                            <td class="mb-3">
+                                <span>
+                                    {{ $contact['email'] }}
+                                </span>
+                            </td>
+                            <td>
+                                <button class="btn btn-danger filled" wire:click="removeContact({{ $index }})">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash-circle" viewBox="0 0 16 16">
+                                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
+                                        <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8"/>
+                                    </svg>
+                                </button>
+                            </td>
+                        </tr>
+                        @endForeach
+                    </tbody>
+                </thead>
+            </table>
+        </div>
+    </div>
+</div>
